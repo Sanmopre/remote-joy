49d3448628574f8c16cd24380da8677fa6b023e6
sanmopre <moliner.santiago@gmail.com>
2024-12-14 13:05:46 +0100

    Added tabulate dependency and created first joystick data table

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ UDP protobuf joystick data sender
 # libs used
 CLI11     v2.4.2   (https://github.com/CLIUtils/CLI11/releases/tag/v2.4.2)
 Protobuf  v29.1    (https://github.com/protocolbuffers/protobuf/releases/tag/v29.1)
-SDL2      V2.30.10 (https://github.com/libsdl-org/SDL/releases/tag/release-2.30.10)
+SDL2      v2.30.10 (https://github.com/libsdl-org/SDL/releases/tag/release-2.30.10)
+tabulate  v1.5     (https://github.com/p-ranav/tabulate/releases/tag/v1.5)
--- remote-joy-exec/src/main.cpp
+++ remote-joy-exec/src/main.cpp
@@ -10,16 +10,57 @@
 // sdl2
 #include <SDL2/SDL.h>
 
+#include "joystick_data.h"
+
+
 int main(int argc, char **argv)
 {
   CLI::App app("Joystick UDP data sender.");
   CLI11_PARSE(app, argc, argv);
 
-  if (SDL_Init(SDL_INIT_JOYSTICK) != 0)
+  if (SDL_Init(SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER) != 0)
   {
     std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
     return -1;
   }
+  SDL_JoystickEventState(SDL_ENABLE);
+  std::vector<JoystickData> joysticks;
+
+  int numJoysticks = SDL_NumJoysticks();
+  std::cout << "Number of joysticks connected: " << numJoysticks << std::endl;
+
+  for (int i = 0; i < numJoysticks; ++i) {
+    JoystickData data;
+    data.index = i;
+
+    // Open joystick
+    SDL_Joystick* joystick = SDL_JoystickOpen(i);
+    if (!joystick) {
+      std::cerr << "Failed to open joystick " << i << ": " << SDL_GetError() << std::endl;
+      continue;
+    }
+
+    // Retrieve information
+    data.name = SDL_JoystickName(joystick) ? SDL_JoystickName(joystick) : "Unknown";
+    data.buttonCount = SDL_JoystickNumButtons(joystick);
+    data.axisCount = SDL_JoystickNumAxes(joystick);
+    data.hatCount = SDL_JoystickNumHats(joystick);
+    data.ballCount = SDL_JoystickNumBalls(joystick);
+
+    // Get Vendor and Product IDs (if supported)
+    SDL_JoystickGUID guid = SDL_JoystickGetGUID(joystick);
+    char guidString[33];
+    SDL_JoystickGetGUIDString(guid, guidString, sizeof(guidString));
+    data.vendorID = guidString;  // Use GUID as a general identifier
+
+    // Add to list
+    joysticks.push_back(data);
+
+    // Close joystick
+    SDL_JoystickClose(joystick);
+  }
+
+  renderJoysticksInfo(joysticks);
 
   SDL_Quit();
 
